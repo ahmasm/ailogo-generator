bb96f3e467c4b3b4425acf0a07029e294dcec6db
ahmasm <ahmasmbkts@gmail.com>
2025-12-05 10:13:32 +0300

    chore: add ESLint config, clean up README

---
+++ app/eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['node_modules/', '.expo/', 'dist/', 'babel.config.js'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-require-imports': 'off', // React Native uses require() for images
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+    },
+  },
+];
